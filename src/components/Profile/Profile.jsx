0c582c5d84c610cc37569da460ce18701c0516a3
tiwaripalak245 <tiwaripalak836@gmail.com>
2024-08-07 23:05:03 +0530

    latest changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,46 +1,45 @@
 import React from "react";
 import cv from "../../assets/shubham.pdf";
-import profileImg from "../../assets/profile.jpg"
+import profileImg from "../../assets/profile.jpg";
+import "../Profile/profile.css";
 
 const Profile = () => {
   return (
     <div
-      className="card shadow-lg compact bg-base-100 mt-3 rounded-2 border-0"
+      className="card shadow-lg compact bg-base-100 mt-3 rounded-2 border-0 d-flex align-items-center justify-content-center"
       style={{ height: "20rem" }}
     >
-      <div className="grid place-items-center py-8">
-      <div className="avatar opacity-90">
-    <div className="mb-8 rounded-full w-20 h-20 ring ring-primary ring-offset-base-100 ring-offset-2 overflow-hidden">
-      {/* <img
-        src={profileImg}
-        alt="shubham ruwasia"
-        className="profile"
-        style={{width: "100px" }}
-      /> */}
-    </div>
-  </div>
+      <div className="text-center py-8">
+        <div className="avatar opacity-90 mb-4">
+          {/* <div className="rounded-circle overflow-hidden" style={{ width: "100px", height: "100px" ,objectFit: "cover"}}>
+          <img
+            src={profileImg}
+            alt="Shubham Ruwasia"
+            className="img-fluid"
+          />
+        </div> */}
+
+          <div className="avatar mb-4">
+            <img
+              src={profileImg}
+              alt="Shubham Ruwasia"
+              className="rounded-circle"
+              style={{ width: "100px", height: "100px"}}
+            />
+          </div>
+        </div>
         <div className="text-center mx-auto px-8">
           <h5 className="font-bold text-2xl">
             <span className="text-base-content opacity-70">
-              SHUBHAM RUWASIA
+              Shubham Ruwasia
             </span>
           </h5>
           <div className="mt-3 text-base-content text-opacity-60 font-mono">
-            ⚡Senior Software Engineer | Java | Spring Boot | Microservices⚡
+            ⚡ Senior Software Engineer | Java | Spring Boot | Microservices ⚡
           </div>
         </div>
-        {/* <a
-          href="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
-          target="_blank"
-          className="btn btn-outline btn-sm text-xs mt-6 opacity-50"
-          download=""
-          rel="noreferrer"
-        >
-          Download Resume
-        </a> */}
         <a href={cv} download="">
-        <button> Download Resume
-        </button>
+          <button className="download-button">Download Resume</button>
         </a>
       </div>
     </div>
